0342433960b76f9fddc8488710d07ea0a35c07f0
Sunset <gongli20040422@163.com>
2025-03-09 13:53:15 +0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sunset2131.github.io/",
-  base: "/dist",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(
