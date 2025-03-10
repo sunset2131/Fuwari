7189240f3c4140c7a6f30bcb79a0d998e9262c3a
Sunset <gongli20040422@163.com>
2025-03-10 13:10:52 +0800

    HTB-dog

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sunset2131.github.io/",
-  base: "/",
+  site: "https://sunset-blog.top/",
+  // base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(
